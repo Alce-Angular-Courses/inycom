62e62f9376c1a1ae002a98c8d5e3cfb177c21581
alce65 <alejandro.cerezo@evosoft.es>
2021-05-13 16:14:16 +0200

    Cambios iniciales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'iny-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+ /*  template : `
+    <h1>Hola Mundo</h1>
+  `, */
+  styleUrls: ['./app.component.scss'],
+  /* styles: [
+    'p {color: blue}'
+  ] */
 })
 export class AppComponent {
   title = 'inycom';
